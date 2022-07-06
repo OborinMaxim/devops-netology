6080416a497a9641479ad99fde0d8b8393e1d64d
Maxim Oborin <mobr89@mail.ru>
2022-07-06 19:38:33 +0500

    [07-terraform-03-basic] Решение

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,9 +14,38 @@ provider "yandex" {
   zone      = "ru-central1-a"
 }
 
-resource "yandex_compute_image" "Ubuntu" {
-  name       = "Ubuntu_20.04_LTS"
+locals {
+  web_instance_count_map = {
+    stage = 1
+    prod = 2
+  }
+}
+
+resource "yandex_compute_image" "Ubuntu-count" {
+  count = local.web_instance_count_map[terraform.workspace]
+
+  name       = "Ubuntu_20.04_LTS-count-${count.index + 1}"
   source_url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
-  description = "Ubuntu 20.04 LTS image for 07 module"
+  description = "Ubuntu 20.04 LTS image for 07 module made through count"
+  os_type = "LINUX"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+locals {
+  instances = {
+    stage = "1"
+    prod = ["1", "2"]
+  }
+}
+
+resource "yandex_compute_image" "Ubuntu-for-each" {
+  for_each = toset(local.instances[terraform.workspace])
+
+  name       = "Ubuntu_20.04_LTS-for-each-${each.value}"
+  source_url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+  description = "Ubuntu 20.04 LTS image for 07 module made through for_each"
   os_type = "LINUX"
 }
